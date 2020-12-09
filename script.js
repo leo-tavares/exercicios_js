910e0098786075faa9c506ab3db922eb6e6f870c
leo-tavares <leonardotavaresdev@gmail.com>
2020-12-09 18:00:10 -0300

    função de saudação - customizada
    Ao clicar em enviar pegamos a valor do input (não vazio) e adicionamos uma menssagem de saudação com o nome informado
    
    Foi feito uma "troca" de component, no caso é feito a remoção do elemento input, e criado um novo elemento span com a mensagem de saudação
    
    O botão tem o texto substituido por "editar" e quando clica novamento mudamos o elemento de span para input e o usuário pode editar o valor infromado

--- script.js
+++ script.js
@@ -31,3 +31,39 @@ btnSqrt.onclick = function () {
 	var result = Math.sqrt(getNumericInputValue(), 2);
 	setNumeticInputValue(result);
 };
+
+var btnSaudacao = document.getElementById("btn-saudacao");
+var userName = "";
+btnSaudacao.onclick = function () {
+	if (btnSaudacao.innerText === "Enviar") {
+		var inputName = document.getElementById("input-name");
+		if (!inputName.value) return;
+		userName = inputName.value;
+		var salutation = `Olá ${userName}! Bem vindo a bordo \u{1F6A2}`;
+
+		var salutationElement = document.createElement("span");
+		salutationElement.id = "user-name";
+		salutationElement.style.marginRight = "0.25rem";
+		salutationElement.innerText = salutation;
+
+		inputName.remove();
+
+		var parent = btnSaudacao.parentNode;
+		parent.insertBefore(salutationElement, btnSaudacao);
+		btnSaudacao.innerText = "Editar";
+	} else if (btnSaudacao.innerText === "Editar") {
+		var inputName = document.createElement("input");
+		inputName.id = "input-name";
+		inputName.type = "text";
+		inputName.placeholder = "Qual é o seu nome?";
+		inputName.style.marginRight = "0.25rem";
+		inputName.value = userName;
+
+		var userNameElement = document.getElementById("user-name");
+		userNameElement.remove();
+
+		var parent = btnSaudacao.parentNode;
+		parent.insertBefore(inputName, btnSaudacao);
+		btnSaudacao.innerText = "Enviar";
+	}
+};
